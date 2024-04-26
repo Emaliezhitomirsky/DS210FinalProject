3662e66942e00156055eea2202c76ceef8698e78
Emaliezhitomirsky <144946611+Emaliezhitomirsky@users.noreply.github.com>
2024-04-26 13:07:35 -0400

    Commit

--- src/main.rs
+++ src/main.rs
@@ -29,8 +29,35 @@ impl NameData {
         }
     }
 }
+
+#[derive(Debug)]
+struct Edge { source: usize, target: usize }
+
+#[derive(Debug)]
+struct Graph { nodes: Vec<NameData>, edges: Vec<Edge> }
+
+impl Graph {
+    fn new() -> Self {
+        Graph { nodes: Vec::new(), edges: Vec::new() }
+    }
+
+    fn add_node(& mut self, node: NameData) { self.nodes.push(node); }
+
+    fn add_edge(&mut self, souce: usize, target: usize) { self.edges.push(Edge { source, target }); }
+
+    fn node_count(&self) -> usize { self.nodes.len() }
+
+    fn get_node(&self, index: usize) -> Option<&NameData> { self.nodes.get(index) }
+
+    fn dfs(&self, node: usize, visited: &mut Vec<bool>) {
+        visited[node] = true;
+        for edge in &self.edges {
+            if edge.source == node && !visited[edge.target] {
+                self.dfs(edge.target, visited);
+            }
+        }
+    }
+}
 fn main() {
     
 }
-
-// Test test test 
